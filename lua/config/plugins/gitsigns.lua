05fc04bad86055fe66e167812713252b52d30afb
Karandeep Tiwana <karandeep.tiwana@outlook.com>
2025-02-03 20:47:33 -0800

    add keymappings to gitsigns

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -2,5 +2,8 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require('gitsigns').setup()
+
+    vim.keymap.set("n", "<space>gp", ":Gitsigns preview_hunk<CR>")
+    vim.keymap.set("n", "<space>gt", ":Gitsigns toggle_current_line_blame<CR>")
   end
 }
