5db0fad72a52293febc82010ed495d12c6bb84a0
Karandeep Tiwana <karandeep.tiwana@outlook.com>
2026-01-10 21:01:01 -0800

    added tabs plugin and buffer navigation

--- lua/config/core/mappings.lua
+++ lua/config/core/mappings.lua
@@ -24,12 +24,12 @@ vim.api.nvim_set_keymap('n', '<leader>fq', ':q!<CR>', opts)
 
 -- Diagnostic keymaps
 vim.keymap.set('n', ']d', function()
-	opts = { count = 1, float = true }
-	vim.diagnostic.jump(opts)
+  opts = { count = 1, float = true }
+  vim.diagnostic.jump(opts)
 end, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', '[d', function()
-	opts = { count = -1, float = true }
-	vim.diagnostic.jump(opts)
+  opts = { count = -1, float = true }
+  vim.diagnostic.jump(opts)
 end, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
@@ -38,8 +38,22 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
 vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
 
--- Tab Navigations
-vim.api.nvim_set_keymap('n', '<Space>t', ':tabnew ', opts)
-vim.api.nvim_set_keymap('n', '<Space>T', ':tabclose<CR>', opts)
-vim.keymap.set('n', '<Tab>', ':tabn<CR>', opts) --  go to next tab
-vim.keymap.set('n', '<S-Tab>', ':tabp<CR>', opts)
+-- buffer Navigations
+vim.api.nvim_set_keymap('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', opts)
+vim.api.nvim_set_keymap('n', '<Tab>', '<Cmd>BufferNext<CR>', opts)
+
+vim.api.nvim_set_keymap('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+
+vim.api.nvim_set_keymap('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+
+vim.api.nvim_set_keymap('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+vim.api.nvim_set_keymap('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
